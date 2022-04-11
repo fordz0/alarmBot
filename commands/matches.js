1271e684899e5f14216472c7c81982bde237afce
ben <ben@pengu1n.dev>
2022-04-11 03:24:15 +0100

    matches command implemented

--- commands/matches.js
+++ commands/matches.js
@@ -10,9 +10,35 @@ module.exports.run = async (client, msg, args) => {
     const userName = parsedArgs.join(' ').split('#')[0]
     const userTag = parsedArgs.join(' ').split('#')[1]
 
-    async function fetchMatches(name, tag) {
-        const matches = await ValorantAPI.getMatches("v3", "na", name, tag);
-        console.log(matches);
+    async function fetchMatches(region, name, tag) {
+        const matches = await ValorantAPI.getMatches(region, name, tag);
+        
+        let matchAccount1
+        let matchAccount2
+        let matchAccount3
+
+        try {
+            matches.data[0].players.all_players.forEach(element => {
+                if (element.name.toLowerCase() == name.toLowerCase() && element.tag.toLowerCase() == tag.toLowerCase()) {
+                    matchAccount1 = element
+                }
+            })
+
+            matches.data[1].players.all_players.forEach(element => {
+                if (element.name.toLowerCase() == name.toLowerCase() && element.tag.toLowerCase() == tag.toLowerCase()) {
+                    matchAccount2 = element
+                }
+            })
+
+            matches.data[2].players.all_players.forEach(element => {
+                if (element.name.toLowerCase() == name.toLowerCase() && element.tag.toLowerCase() == tag.toLowerCase()) {
+                    matchAccount3 = element
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
         if (matches.status == 200) {
             request(url, options, (error, res, body) => {
                 if (error) {
@@ -20,6 +46,15 @@ module.exports.run = async (client, msg, args) => {
                 };
 
                 if (!error && res.statusCode == 200) {
+                    const embed = new Discord.MessageEmbed()
+                        .setColor(0x3498DB)
+                        .setTitle(name + "'s Match History")
+                        .setDescription("Previous Matches")
+                        .addField(matches.data[0].metadata.map + " " + matches.data[0].metadata.game_start_patched, "``` K/D/A " + matchAccount1.stats.kills + "/" + matchAccount1.stats.deaths + "/" + matchAccount1.stats.assists + "```") //matches.data[0])
+                        .addField(matches.data[1].metadata.map + " " + matches.data[1].metadata.game_start_patched, "``` K/D/A " + matchAccount2.stats.kills + "/" + matchAccount2.stats.deaths + "/" + matchAccount2.stats.assists + "```") //matches.data[1])
+                        .addField(matches.data[2].metadata.map + " " + matches.data[2].metadata.game_start_patched, "``` K/D/A " + matchAccount3.stats.kills + "/" + matchAccount3.stats.deaths + "/" + matchAccount3.stats.assists + "```") //matches.data[2])
+                    
+                    msg.reply({ embeds: [embed] })
                     console.log(matches);
                 }
             })
@@ -27,7 +62,7 @@ module.exports.run = async (client, msg, args) => {
             msg.reply(`There was an error finding that player.`);
         }
     }
-    fetchMatches(userName, userTag);
+    fetchMatches("na", userName, userTag);
 }
 
 module.exports.help = {
