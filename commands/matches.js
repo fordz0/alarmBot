2e8ae59bab851372f7dce764d018269a22fe918a
ben <ben@pengu1n.dev>
2022-04-12 15:19:59 +0100

    fixed redundant code

--- commands/matches.js
+++ commands/matches.js
@@ -3,8 +3,6 @@ const ValorantAPI = require('unofficial-valorant-api');
 const request = require('request');
 
 module.exports.run = async (client, msg, args) => {
-    let url = "https://valorant-api.com/v1/competitivetiers";
-    let options = {json: true};
     const parsedArgs = args
     parsedArgs.shift()
     const userName = parsedArgs.join(' ').split('#')[0]
@@ -41,25 +39,18 @@ module.exports.run = async (client, msg, args) => {
         }
 
         if (matches.status == 200) {
-            request(url, options, (error, res, body) => {
-                if (error) {
-                    return console.log(error)
-                };
-
-                if (!error && res.statusCode == 200) {
-                    const embed = new Discord.MessageEmbed()
-                        .setColor(0x3498DB)
-                        .setTitle(name + "'s Match History")
-                        .setDescription("Previous Matches")
-                        .addField(matches.data[0].metadata.map + " " + matches.data[0].metadata.game_start_patched, "``` K/D/A " + matchAccount1.stats.kills + "/" + matchAccount1.stats.deaths + "/" + matchAccount1.stats.assists + "```") //matches.data[0])
-                        .addField(matches.data[1].metadata.map + " " + matches.data[1].metadata.game_start_patched, "``` K/D/A " + matchAccount2.stats.kills + "/" + matchAccount2.stats.deaths + "/" + matchAccount2.stats.assists + "```") //matches.data[1])
-                        .addField(matches.data[2].metadata.map + " " + matches.data[2].metadata.game_start_patched, "``` K/D/A " + matchAccount3.stats.kills + "/" + matchAccount3.stats.deaths + "/" + matchAccount3.stats.assists + "```") //matches.data[2])
-                        .setThumbnail(account.data.card.small)
+            if (!error && res.statusCode == 200) {
+                const embed = new Discord.MessageEmbed()
+                    .setColor(0x3498DB)
+                    .setTitle(name + "'s Match History")
+                    .setDescription("Previous Matches")
+                    .addField(matches.data[0].metadata.map + " " + matches.data[0].metadata.game_start_patched, "``` K/D/A " + matchAccount1.stats.kills + "/" + matchAccount1.stats.deaths + "/" + matchAccount1.stats.assists + "```") //matches.data[0])
+                    .addField(matches.data[1].metadata.map + " " + matches.data[1].metadata.game_start_patched, "``` K/D/A " + matchAccount2.stats.kills + "/" + matchAccount2.stats.deaths + "/" + matchAccount2.stats.assists + "```") //matches.data[1])
+                    .addField(matches.data[2].metadata.map + " " + matches.data[2].metadata.game_start_patched, "``` K/D/A " + matchAccount3.stats.kills + "/" + matchAccount3.stats.deaths + "/" + matchAccount3.stats.assists + "```") //matches.data[2])
+                    .setThumbnail(account.data.card.small)
                     
-                    msg.reply({ embeds: [embed] })
-                    console.log(matches);
+                msg.reply({ embeds: [embed] })
                 }
-            })
         } else {
             msg.reply(`There was an error finding that player.`);
         }
