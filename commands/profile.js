e29872ad4091c3677b47ef5b5db26f1f3e0548b2
ben <ben@pengu1n.dev>
2022-04-12 15:19:46 +0100

    fixed redundant code

---
+++ commands/profile.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const ValorantAPI = require("unofficial-valorant-api");
+const request = require('request');
+
+module.exports.run = async (client, msg, args) => {
+    let url = "https://valorant-api.com/v1/competitivetiers";
+    
+}
+
+module.exports.help = {
+    name: "profile"
+}
