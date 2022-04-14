63fee3657bbd245236e6c65f06614adab8834c7f
ben <ben@pengu1n.dev>
2022-04-14 19:24:58 +0100

    fix updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, msg, args) => {
-    if(!(args.length > 1))  return;
-
+    if(!(args.length > 1)) {
     let role = msg.guild.roles.cache.find(role => role.name === 'AlarmBot Updates')
     msg.member.roles.add(role)
 
     msg.react("✅")
+    }
 }
 
 module.exports.help = {
